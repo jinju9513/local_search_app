073fba2b3341c5c3b14c28087f768805c5664805
jinju9513 <wjswlswn99@naver.com>
2025-04-22 09:15:03 +0900

    feat: viewmodel 연결 리스트 보이게, detailpage link넘겨주기

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,55 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:local_search_app/ui/detail/detail_page.dart';
+import 'package:local_search_app/ui/home/home_page_view_model.dart';
 
-class HomePage extends StatelessWidget {
-  final List<Map<String, String>> items = [
-    {
-      'title': '스타벅스 강남점',
-      'category': '카페',
-      'roadAddress': '서울 강남구 테헤란로 123',
-    },
-    {
-      'title': '이디야커피',
-      'category': '커피숍',
-      'roadAddress': '서울 서초구 서초대로 456',
-    },
-  ];
+class HomePage extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+  
+}
 
+class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final items =ref.watch(locationProvider);
+
     return GestureDetector(
       onTap: () {
-        FocusScope.of(context).unfocus(); //키보드 내려가게 하는
+        FocusScope.of(context).unfocus(); // 키보드 내려가게 하는
       },
       child: Scaffold(
         appBar: AppBar(
           actions: [
             GestureDetector(
-                onTap: () {},
-                //버튼의 터치영역은 44 디바이스 픽셀 이상으로 해줘야함!!(UX)
-                child: Container(
-                    width: 50,
-                    height: 50,
-                    //컨테이너에 배경색이 없으면 자녀위젯에만 터치 이벤트가 적용됨
-                    color: Colors.transparent,
-                    child: Icon(Icons.my_location))),
+              onTap: () {},
+              child: Container(
+                width: 50,
+                height: 50,
+                color: Colors.transparent,
+                child: Icon(Icons.my_location),
+              ),
+            ),
           ],
           title: TextField(
+            onSubmitted: (value) {
+              ref.read(locationProvider.notifier).search(value); // 검색실행
+            },
             maxLines: 1,
             decoration: InputDecoration(
-                hintText: '검색어를 입력해주세요',
-                border: MaterialStateOutlineInputBorder.resolveWith((states) {
-                  print(states);
-                  if (states.contains(WidgetState.focused)) {
-                    return OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.black),
-                    );
-                  }
+              hintText: '검색어를 입력해주세요',
+              border: MaterialStateOutlineInputBorder.resolveWith((states) {
+                if (states.contains(WidgetState.focused)) {
                   return OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey),
+                    borderSide: BorderSide(color: Colors.black),
                   );
-                })),
+                }
+                return OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.grey),
+                );
+              }),
+            ),
           ),
         ),
         body: ListView.builder(
@@ -57,27 +58,40 @@ class HomePage extends StatelessWidget {
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
-            return Container(
-              margin: EdgeInsets.only(bottom: 16),
-              padding: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.grey.shade300),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(item['title'] ?? '',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                  SizedBox(height: 4),
-                  Text(item['category'] ?? '',
-                      style: TextStyle(color: Colors.grey[700])),
-                  SizedBox(height: 4),
-                  Text(item['roadAddress'] ?? '',
-                      style: TextStyle(color: Colors.grey[600])),
-                ],
+            return GestureDetector(
+              onTap: () {
+                final link = item.link;
+                if (link.startsWith('http')) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => DetailPage(link:link),
+                    ),
+                  );
+                }
+              },
+              child: Container(
+                margin: EdgeInsets.only(bottom: 16),
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.grey.shade300),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(item.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                    SizedBox(height: 4),
+                    Text(item.category,
+                        style: TextStyle(color: Colors.grey[700])),
+                    SizedBox(height: 4),
+                    Text(item.roadAddress,
+                        style: TextStyle(color: Colors.grey[600])),
+                  ],
+                ),
               ),
             );
           },
